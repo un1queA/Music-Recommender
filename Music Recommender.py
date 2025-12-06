02858cfdd555f0c56ef68b8fcd526e4277f6f392
un1queA <aroncheong29@gmail.com>
2025-12-06 21:23:26 +0800

    Update Music Recommender.py

--- Music Recommender.py
+++ Music Recommender.py
@@ -9,7 +9,8 @@ from typing import List, Dict, Optional
 from youtube_search import YoutubeSearch
 
 # Import LangChain components - updated for DeepSeek
-from langchain_openai import ChatOpenAI  # Compatible with OpenAI format
+
+from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain
 
@@ -403,6 +404,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
